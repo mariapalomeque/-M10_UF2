bb311c7c1dc051ea301ad90f2383aecd6098ec4b
mariapalomeque <146199622+mariapalomeque@users.noreply.github.com>
2024-02-15 13:10:39 +0100

    Repaso

--- REPÀS/exercici32.py
+++ REPÀS/exercici32.py
@@ -1,14 +1,10 @@
 def calcular_quadrats(lista_numeros):
-    try:
-        # Verificar que sean 10 numeros
-        if len(lista_numeros) != 10:
-            raise ValueError("La lista debe contener exactamente 10 numeros.")
 
-        # Calcular cuadrados 
-        lista_quadrats = [numero ** 2 for numero in lista_numeros]
+    # Verificar que sean 10 numeros
+    if len(lista_numeros) != 10:
+        print("La lista debe contener exactamente 10 numeros.")
 
-        return lista_quadrats
+    # Calcular cuadrados 
+    lista_quadrats = [numero ** 2 for numero in lista_numeros]
 
-    except ValueError as error:
-        print(f"Error: {error}")
-
+    return lista_quadrats
